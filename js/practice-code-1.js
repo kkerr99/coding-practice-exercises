75dd1ccf2a858faa76d79a7b57bfac77653546bf
Kris Kerr <kkerr99@gmail.com>
2018-02-01 18:52:15 -0800

    Add nthroot()

--- js/practice-code-1.js
+++ js/practice-code-1.js
@@ -1,1 +1,14 @@
 // JavaScript practice examples
+
+/*
+Write a JavaScript function to calculate the nth root of a number.
+
+Test Data :
+console.log(nthroot(64, 2));
+8
+console.log(nthroot(64, -2));
+0.125
+*/
+function nthroot (y, n) {
+  return Math.pow(y, (1/n));
+}
