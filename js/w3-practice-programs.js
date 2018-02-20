2af0f5b5a1d4d6ff7fb3c9d996b9c1386912a343
Kris Kerr <kkerr99@gmail.com>
2018-02-19 20:20:28 -0800

    Add #17--Number letter counts, incomplete

--- js/w3-practice-programs.js
+++ js/w3-practice-programs.js
@@ -1507,3 +1507,74 @@ function longestCollatzSequence(upperLimit) {
 
 var UPPERLIMIT = 1000000;
 console.log(longestCollatzSequence(UPPERLIMIT));
+
+/*
+https://projecteuler.net/problem=17
+
+#17--Number letter counts
+
+If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out
+in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and 
+forty-two) contains 23 letters and 115 (one hundred and fifteen)
+contains 20 letters. The use of "and" when writing out numbers is in
+compliance with British usage.
+*/
+
+function parseNumber(num, wordArray) {
+  var xString = num.toString();
+  var mostSignificantDigit = 0;
+  for (i = xString.length - 1; i >= 0; i--) {
+    mostSignificantDigit = Math.trunc(num / Math.pow(10, i));
+    if (mostSignificantDigit >= 1) {
+      switch(i) {
+        case 3:
+          if (mostSignificantDigit == 1) {
+            wordArray[(xString.length-1) - i] = "one thousand";
+          }
+        break;
+
+      }
+    }
+
+    switch(i) {
+      case 3:
+        if (mostSignificantDigit == 1) {
+          wordArray[(xString.length-1) - i] = "one thousand";
+        }
+        break;
+      case 2:
+        if (mostSignificantDigit >= 1) {
+
+        }
+        break;
+      case 1:
+        if (mostSignificantDigit >= 1) {
+
+        }
+        code block
+        break;
+      case 0:
+        code block
+        break;
+      default:
+        code block
+    }
+  }
+}
+
+numberLetterCounter(number) {
+  var wordArray = [];
+  parseNumber(number, wordArray);
+  var letterSum = countLettersInWords(wordArray);
+  return letterSum;
+}
+
+
+var NUMBER = 1000;
+console.log(numberLetterCounter(MAXNUMBER));
