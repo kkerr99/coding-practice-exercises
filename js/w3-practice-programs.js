5b0f001c9ebb9a17692e0e110f25eadbf8154815
Kris Kerr <kkerr99@gmail.com>
2018-02-20 17:42:09 -0800

    Add Project Euler #18--Maximum path sum I

--- js/w3-practice-programs.js
+++ js/w3-practice-programs.js
@@ -1520,7 +1520,7 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out
 in words, how many letters would be used?
 
 
-NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and 
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
 forty-two) contains 23 letters and 115 (one hundred and fifteen)
 contains 20 letters. The use of "and" when writing out numbers is in
 compliance with British usage.
@@ -1578,3 +1578,104 @@ numberLetterCounter(number) {
 
 var NUMBER = 1000;
 console.log(numberLetterCounter(MAXNUMBER));
+
+/*
+https://projecteuler.net/problem=18
+
+#18--Maximum path sum I
+
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+
+Strategy:
+
+Enter triangle data into an array of arrays
+Use recursive function
+function maxPathSum(x, y) {
+  var maxSum = 0;
+  function pathSum(x, y, sum)
+    if (y == yArray.length -1)
+      sum += [x,y];
+      if sum > maxSum
+        maxSum= sum;
+      return 1;
+    else
+      return (pathSum(x, y+1, array[x,y] + sum) || pathSum(x+1, y+1, array[x,y] + sum)
+  })
+  pathSum(0, 0, 0);
+  return maxSum;
+}
+
+*/
+
+var triangleArray1 = [
+  [3],
+  [7, 4],
+  [2, 4, 6],
+  [8, 5, 9, 3]
+];
+
+var triangleArray2 = [
+  [75],
+  [95, 64],
+  [17, 47, 82],
+  [18, 35, 87, 10],
+  [20, 4, 82, 47, 65],
+  [19, 1, 23, 75, 3, 34],
+  [88, 2, 77, 73, 7, 63, 67],
+  [99, 65, 4, 28, 6, 16, 70, 92],
+  [41, 41, 26, 56, 83, 40, 80, 70, 33],
+  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+  [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+  [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+];
+
+function maxPathSum(x, y, triArray) {
+  var maxSum = 0;
+  function pathSum(x, y, sum) {
+    sum += triArray[x][y];
+    //console.log("x: " + x + " y: " + y + " sum: " + sum);
+    if (x == triArray.length-1) {
+      if (sum > maxSum) {
+        maxSum = sum;
+      }
+      return 1;
+    }
+    else {
+      return pathSum(x+1, y, sum) && pathSum(x+1, y+1, sum);
+    }
+  }
+  pathSum(0, 0, 0);
+  return maxSum;
+}
+
+console.log(maxPathSum(0, 0, triangleArray2));
