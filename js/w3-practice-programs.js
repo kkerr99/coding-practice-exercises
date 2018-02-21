e740e2c37bc41a268b54b8341dfc78cc7bfcfffd
Kris Kerr <kkerr99@gmail.com>
2018-02-21 13:20:55 -0800

    Add Project Euler #21--Amicable numbers, incomplete

--- js/w3-practice-programs.js
+++ js/w3-practice-programs.js
@@ -1783,3 +1783,63 @@ function sundayCounter() {
 
 console.log(sundayCounter());
 // 171
+
+/*
+https://projecteuler.net/problem=21
+
+#21--Amicable numbers
+
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide
+evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+
+Strategy:
+var n = 2;
+var amicableNumberSum = 0;
+while (amicableNumberCount <= 10000)
+  var a = n;
+  var b = d(a);
+
+  if ((d(b) == a) && (a !== b))
+    amicableNumberSum += a;
+
+return amicableNumberSum
+*/
+
+function d(num) {
+  var sum = 0;
+  for (var i = 1; i < num; i++) {
+    if (num % i == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+function amicableNumberSum(limit) {
+  var n = 2;
+  var amicableNumberSum = 0;
+
+  while (true) {
+    var a = n;
+    var b = d(a);
+
+
+    if ((d(b) == a) && (a !== b)) {
+      if ((a >= limit) || (b >= limit)) break;
+      amicableNumberSum += (a + b);
+      console.log(a, b);
+    }
+    n++;
+  }
+
+  return amicableNumberSum;
+}
+
+
+var LIMIT = 10000;
+console.log(amicableNumberSum(LIMIT));
